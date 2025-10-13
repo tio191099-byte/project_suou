07b935abc64589c58142080eb4b4eecada52b174
tio191099-byte <tio191099@gmail.com>
2025-10-13 04:34:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const tombolsapa = document.getElementById('sapaButton');
 // 2. Tambahkan 'event listener' untuk aksi 'click'
 tombolSapa.addEventListener('click', function() {
   // 3. Jalankan kode ini ketika tombol di-klik
-  alert('Halo! Terima kasih sudah berkunjung!');
+('alert	www.youtube.com/@Aether1465');
 });
