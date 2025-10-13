c06cf179e14cb3940fc327f68102ed7e563fe586
tio191099-byte <tio191099@gmail.com>
2025-10-14 06:55:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // 1. Pilih tombol berdasarkan ID-nya
-const youtube = document.getElementById('youtube');
+const tombolsapa = document.getElementById('sapaButton');
 // 2. Tambahkan 'event listener' untuk aksi 'click'
 youtube.addEventListener('click', function() {
   // 3. Jalankan kode ini ketika tombol di-klik
